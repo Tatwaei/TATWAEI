2ba1a45cc223597ec2b3b2e5cf112ab006f4425a
mnayr <441201220@student.ksu.edu.sa>
2024-02-05 03:01:07 +0300

    main merge conflict?
    صار شي غريب يوم حاولت اسوي بوش، اتوقع عشان انا وسارة عدلنا شوي على المين عشان نختبر

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-<<<<<<< HEAD
 import 'login.dart';
-=======
 import 'homePageStudent.dart';
->>>>>>> 6168b0d8d9893053b106b633c2dfe581301ec34a
 //import 'homePageCoordinator.dart';
 //import 'homePageAdmin.dart';
 
